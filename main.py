6990d944b1ebbde7fa8a32880b9b53ff319978f1
sourabhpatil1010 <sourabhpatil0543@gmail.com>
2026-05-06 17:12:13 +0530

    print more funtions

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 
-from app import addition, subtraction
+from app import addition, division, multiplication, subtraction
 
 a = 10
 b = 20
 print("Addition:", addition(a, b))
 print("Subtraction:", subtraction(a, b))
+print("Multiplication:", multiplication(a, b))
+print("Division:", division(a, b))
