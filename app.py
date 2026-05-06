23e52c84a0c8c45204dffc293c589dbb203c2cd2
sourabhpatil1010 <sourabhpatil0543@gmail.com>
2026-05-06 17:08:52 +0530

    add multiplication funtion

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 def addition(a, b, c):
     return a + b + c
 
-def subtraction(a, b):
-    return a - b
+def subtraction(a, b, c):
+    return a - b - c
 
 def division(a, b):
     if b == 0:
         raise ValueError("Cannot divide by zero")
     return a / b
+
+def multiplication(a, b):
+    return a * b
